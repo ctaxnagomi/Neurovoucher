1d6264a26de2243a09b8177622a535c65dfdad34
CTAXNAGOMI <wan.mohd.azizi.seggaf@gmail.com>
2025-11-25 15:35:10 +0800

    Core [MODIFY] App.tsx Size increased. [MODIFY] index.html Size increased. [MODIFY] package.json Size increased (likely new dependency). Components [MODIFY] Layout.tsx Significant update (size increased). Contexts [NEW] LiveAgentContext.tsx New context file for Live Agent functionality.

--- App.tsx
+++ App.tsx
@@ -9,24 +9,27 @@ import { LiveAgent } from './pages/LiveAgent';
 import { ReceiptEditor } from './pages/ReceiptEditor';
 import { LHDNLetterGenerator } from './pages/LHDNLetterGenerator';
 import { Settings } from './pages/Settings';
+import { LiveAgentProvider } from './contexts/LiveAgentContext';
 
 function App() {
   return (
-    <HashRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/voucher" element={<VoucherGenerator />} />
-          <Route path="/vouchers" element={<VoucherList />} />
-          <Route path="/lhdn-letter" element={<LHDNLetterGenerator />} />
-          <Route path="/chat" element={<ChatAssistant />} />
-          <Route path="/live" element={<LiveAgent />} />
-          <Route path="/editor" element={<ReceiptEditor />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </Layout>
-    </HashRouter>
+    <LiveAgentProvider>
+      <HashRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/voucher" element={<VoucherGenerator />} />
+            <Route path="/vouchers" element={<VoucherList />} />
+            <Route path="/lhdn-letter" element={<LHDNLetterGenerator />} />
+            <Route path="/chat" element={<ChatAssistant />} />
+            <Route path="/live" element={<LiveAgent />} />
+            <Route path="/editor" element={<ReceiptEditor />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </Layout>
+      </HashRouter>
+    </LiveAgentProvider>
   );
 }
 
